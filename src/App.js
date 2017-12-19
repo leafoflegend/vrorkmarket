43b9f6a2fa6ea9e0dbf18bcf0c3f58fb55086b79
Eliot Szwajkowski <eliotpszw@gmail.com>
2017-12-19 12:25:00 -0500

    aframe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import TestVR from './components/testVR';
 
 class App extends Component {
   render() {
     return (
       <Provider store={ store }>
-        <div className="App">
-          Hi.
+        <div
+          className="App"
+          style={{
+            width: '100%',
+            height: '1200px',
+          }}
+        >
+          <TestVR />
         </div>
       </Provider>
     );
